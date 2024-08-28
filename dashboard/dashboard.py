aecdedbe86eeb14acb7c800c1616e97ad8b9d066
najlasjad <147910810+najlasjad@users.noreply.github.com>
2024-08-28 15:16:52 +0700

    new changes.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_csv('./all_data.csv')  
+df = pd.read_csv('dashboard/all_data.csv')  
 df['dteday'] = pd.to_datetime(df['dteday'])
 
 data = {
